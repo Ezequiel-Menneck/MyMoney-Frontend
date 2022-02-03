9a6f284cb1a3cd2bd5e8293c55514fa1f2d91579
Ezequiel Menneck <zeeeee.peeeeetryyy@gmail.com>
2022-02-03 00:03:03 -0300

    feat: new tabs

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 
 import ContentHeader from "../common/template/contentHeader";
 import Content from "../common/template/content";
@@ -6,8 +8,15 @@ import Tabs from "../common/tab/tabs";
 import TabsHeader from "../common/tab/tabsHeader";
 import TabsContent from "../common/tab/tabsContent";
 import TabHeader from "../common/tab/tabHeader";
+import TabContent from "../common/tab/tabContent";
+import { selectTab, showTabs } from "../common/tab/tabActions";
 
-export default class BillingCycles extends Component {
+class BillingCycles extends Component {
+    componentWillMount() {
+        this.props.selectTab("tabList");
+        this.props.showTabs('tabList', 'tabCreate')
+    }
+
     render() {
         return (
             <div>
@@ -15,13 +24,40 @@ export default class BillingCycles extends Component {
                 <Content>
                     <Tabs>
                         <TabsHeader>
-                            <TabHeader label="Listar" icon="bars" target="tabList" />
-                            <TabHeader label="Incluir" icon="plus" target="tabCreate" />
-                            <TabHeader label="Alterar" icon="pencil" target="tabUpdate" />
-                            <TabHeader label="Excluir" icon="trash-o" target="tabDelete" />
+                            <TabHeader
+                                label=" Listar"
+                                icon="bars"
+                                target="tabList"
+                            />
+                            <TabHeader
+                                label=" Incluir"
+                                icon="plus"
+                                target="tabCreate"
+                            />
+                            <TabHeader
+                                label=" Alterar"
+                                icon="pencil"
+                                target="tabUpdate"
+                            />
+                            <TabHeader
+                                label=" Excluir"
+                                icon="trash-o"
+                                target="tabDelete"
+                            />
                         </TabsHeader>
                         <TabsContent>
-
+                            <TabContent id="tabList">
+                                <h1>Lista</h1>
+                            </TabContent>
+                            <TabContent id="tabCreate">
+                                <h1>Incluir</h1>
+                            </TabContent>
+                            <TabContent id="tabUpdate">
+                                <h1>Alterar</h1>
+                            </TabContent>
+                            <TabContent id="tabDelete">
+                                <h1>Excluir</h1>
+                            </TabContent>
                         </TabsContent>
                     </Tabs>
                 </Content>
@@ -29,3 +65,7 @@ export default class BillingCycles extends Component {
         );
     }
 }
+
+const mapDispatchToProps = (dispatch) =>
+    bindActionCreators({ selectTab, showTabs }, dispatch);
+export default connect(null, mapDispatchToProps)(BillingCycles);
