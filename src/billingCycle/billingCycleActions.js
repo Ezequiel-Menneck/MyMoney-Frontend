2e71187b5e1afab5d6925c7bc14eee2f392ae1fd
Ezequiel Menneck <zeeeee.peeeeetryyy@gmail.com>
2022-02-08 18:52:36 -0300

    Adicionando novo layout ao formulário

--- src/billingCycle/billingCycleActions.js
+++ src/billingCycle/billingCycleActions.js
@@ -1,17 +1,35 @@
-import axios from 'axios'
-const BASE_URL = 'http://localhosT:3003/api'
+import axios from "axios";
+import { toastr } from "react-redux-toastr";
+import { reset as resetForm } from "redux-form";
+import { showTabs, selectTab } from "./../common/tab/tabActions";
+
+const BASE_URL = "http://localhosT:3003/api";
 
 export function getList() {
-    const request = axios.get(`${BASE_URL}/billingCycles`)
+    const request = axios.get(`${BASE_URL}/billingCycles`);
     return {
-        type: 'BILLING_CYCLE_FETCHED',
-        payload: request
-    }
+        type: "BILLING_CYCLE_FETCHED",
+        payload: request,
+    };
 }
 
 export function create(values) {
-    axios.post(`${BASE_URL}/billingCycles`, values)
-    return {
-        type: 'TEMP',
-    }
+    return (dispath) => {
+        axios
+            .post(`${BASE_URL}/billingCycles`, values)
+            .then((res) => {
+                toastr.success("Sucesso", "Operação realizada com sucesso");
+                dispath([
+                    resetForm('billingCycleForm'),
+                    getList(),
+                    selectTab('tabList'),
+                    showTabs('tabList', 'tabCreate')
+                ]);
+            })
+            .catch((e) => {
+                e.response.data.errors.forEach((error) =>
+                    toastr.error("Erro", error)
+                );
+            });
+    };
 }
--- src/main/app.jsx
+++ src/main/app.jsx
@@ -5,6 +5,7 @@ import Header from '../common/template/header'
 import SideBar from '../common/template/siderBar'
 import Footer from '../common/template/footer'
 import Routes from './routes'
+import Messages from '../common/msg/messages'
 
 export default (props) => {
     return (
@@ -15,6 +16,7 @@ export default (props) => {
                 <Routes />
             </div>
             <Footer />
+            <Messages />
         </div>
     );
 };
